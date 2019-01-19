44e2f6c29eb154dae39b5c063088efea36adf16f
Odysseas Samaras <odysseas.samaras@gmail.com>
2019-01-19 21:40:35 +0200

    fix project file classpath

---
+++ test/end-to-end/test/endtoend/auctionsniper/SingleMessageListener.java
@@ -0,0 +1,12 @@
+package test.endtoend.auctionsniper;
+
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.MessageListener;
+import org.jivesoftware.smack.packet.Message;
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+import static org.hamcrest.core
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.hamcrest.core.IsNull.notNullValue;
+
