b5b0ef163dfa9541e9998e529426549952ffb422
Odysseas Samaras <odysseas.samaras@gmail.com>
2019-04-07 08:05:47 +0300

    add sniper portfolio and previously missed state unit tests

---
+++ test/unit/test/auctionsniper/SniperPortfolioTest.java
@@ -0,0 +1,29 @@
+package test.auctionsniper;
+
+import auctionsniper.Auction;
+import auctionsniper.AuctionSniper;
+import auctionsniper.SniperPortfolio;
+import auctionsniper.SniperPortfolio.PortfolioListener;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(JMock.class)
+public class SniperPortfolioTest {
+    private final Mockery context = new Mockery();
+    private final PortfolioListener listener = context.mock(PortfolioListener.class);
+    private final SniperPortfolio portfolio = new SniperPortfolio();
+
+    @Test public void
+    notifiesListenersOfNewSnipers() {
+        final AuctionSniper sniper = new AuctionSniper("item id", null);
+        context.checking(new Expectations() {{
+            oneOf(listener).sniperAdded(sniper);
+        }});
+        portfolio.addPortfolioListener(listener);
+
+        portfolio.addSniper(sniper);
+    }
+}
